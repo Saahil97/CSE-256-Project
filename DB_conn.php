94970c8685d96f6d3b58ec2dd89b540f9845892b
Saahil Afaq <38484758+Saahil97@users.noreply.github.com>
2018-08-05 10:13:27 +0530

    Delete DB_conn.php

--- DB_conn.php
+++
@@ -1,9 +0,0 @@
-<?php
-$db_host = 'localhost';
-$db_user = 'root';
-$db_password = 'tiger';
-$db_name = 'saahil';
-
-$dbh=mysqli_connect($db_host,$db_user,$db_password,$db_name) or die("Error connecting to the database".mysqli_connect_error($dbh));
-
-?>
